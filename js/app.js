0ca98931ce272611549251fc6b043dac42d841e7
Valentin Coppin <valimero.ilou@gmail.com>
2016-06-20 22:41:37 +0200

    add app.js

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+document.getElementById("id_window").innerHTML += '<div class="panel-body">
+					
+					
+					
+					
+						<div class="input-group">
+				<span class="input-group-addon">
+				<input type="checkbox" aria-label="...">
+				</span>
+				<input type="text" class="form-control" aria-label="...">
+				 <div class="input-group-btn">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span class="caret"></span></button>
+        <ul class="dropdown-menu">
+          
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li><a href="#">Separated link</a></li>
+          <li><a href="#" class="label-danger">Delete</a></li>
+        </ul>
+      </div><!-- /btn-group -->
+				</div><!-- /input-group -->
+				
+					
+					</div>';
